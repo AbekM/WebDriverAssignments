2fd4a18d213bdc150599aec76f95197cf598afb1
AbekM <asilbek.makhmudov@outlook.com>
2022-12-21 13:15:43 +0500

    ready to test screenshot

--- src/test/java/com/epam/ta/util/TestListener.java
+++ src/test/java/com/epam/ta/util/TestListener.java
@@ -24,7 +24,7 @@ public class TestListener implements ITestListener {
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-
+        saveScreenshot();
     }
 
     public void onTestFailure(ITestResult iTestResult) {
